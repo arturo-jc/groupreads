63f5a97fecb1f55967bbbafb1495a56f8713e1c4
Arturo Javier-Castellanos <a.javier.castellanos@gmail.com>
2021-12-20 22:04:08 -0600

    Delete unnecessary props from Home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,9 +4,8 @@ import PropTypes from 'prop-types';
 import Landing from "./home/Landing"
 import Dashboard from "./home/Dashboard"
 import { loadUser } from '../actions/authActions';
-import { getGroups } from '../actions/groupActions';
 
-const Home = ({ authState, loadUser, getGroups }) => {
+const Home = ({ authState, loadUser }) => {
     useEffect(() => {
         if (localStorage.token) {
             loadUser();
@@ -19,14 +18,13 @@ const Home = ({ authState, loadUser, getGroups }) => {
 
 Home.propTypes = {
     authState: PropTypes.object.isRequired,
-    loadUser: PropTypes.func.isRequired,
-    getGroups: PropTypes.func.isRequired
+    loadUser: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
     authState: state.auth
 })
 
-const addState = connect(mapStateToProps, { loadUser, getGroups });
+const addState = connect(mapStateToProps, { loadUser });
 
 export default addState(Home);
