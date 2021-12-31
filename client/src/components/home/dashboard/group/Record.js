99f24a4e904327fecbb775c4d492d1f630a04f37
Arturo Javier-Castellanos <a.javier.castellanos@gmail.com>
2021-12-31 01:50:39 -0600

    Make progress into its own component

--- client/src/components/home/dashboard/group/Record.js
+++ client/src/components/home/dashboard/group/Record.js
@@ -4,36 +4,16 @@ import PropTypes from 'prop-types';
 import { connect } from "react-redux";
 import Modal from "../../../Modal";
 import NewPostForm from './record/NewPostForm';
-import NewMarkerForm from './record/NewMarkerForm';
 import NewBookmarkForm from './record/NewBookmarkForm';
 import Items from './record/Items';
-import ProgressBar from "./record/ProgressBar";
-import StatusHistory from './record/StatusHistory';
+import Progress from './record/Progress';
 import { setCurrentRecord } from '../../../../actions/recordActions';
-import { getPostsFor } from '../../../../actions/postActions';
-import { getMarkersFor } from '../../../../actions/markerActions';
-import { getBookmarksFor } from '../../../../actions/bookmarkActions';
-import { addMarker } from '../../../../actions/markerActions';
 
-const Record = ({ recordState, markerState, setCurrentRecord, getMarkersFor, addMarker }) => {
+const Record = ({ recordState, setCurrentRecord }) => {
 
-    const [newMarkerModal, setNewMarkerModal] = useState({show: false})
     const [newBookmarkModal, setNewBookmarkModal] = useState({show: false})
     const [newPostModal, setNewPostModal] = useState({show: false})
-    const [statusHistoryModal, setStatusHistoryModal] = useState({show: false})
 
-    const showNewMarkerModal = () => {
-        setNewMarkerModal({
-            ...newMarkerModal,
-            show: true
-        })
-    }
-    const hideNewMarkerModal = () => {
-        setNewMarkerModal({
-            ...newMarkerModal,
-            show: false
-        })
-    }
     const showNewBookmarkModal = () => {
         setNewBookmarkModal({
             ...newBookmarkModal,
@@ -59,50 +39,16 @@ const Record = ({ recordState, markerState, setCurrentRecord, getMarkersFor, add
         })
     }
 
-    const showStatusHistoryModal = () => {
-        setStatusHistoryModal({
-            ...statusHistoryModal,
-            show: true
-        })
-    }
-
-    const hideStatusHistoryModal = () => {
-        setStatusHistoryModal({
-            ...statusHistoryModal,
-            show: false
-        })
-    }
-
-    const {groupId, recordId} = useParams();
+    const {recordId} = useParams();
     const { current, records } = recordState;
     const record = records.find(record => record._id === recordId);
 
     useEffect(() => {
         if(record){
             setCurrentRecord(record);
-            getMarkersFor(groupId, record);
         }
     }, [record])
-
-    const { markers } = markerState;
-
-    let currentPage = 0;
-    if (markers.length > 0){
-        markers.sort(function(a, b){
-            return new Date(b.date) - new Date(a.date)
-        });
-        currentPage = markers[0].page
-    }
-
-    let completed = 0;
-    if (current && current.book.pageCount){
-        completed = Math.round((currentPage / current.book.pageCount) * 100);
-    }
-
-    const markComplete = () => {
-        addMarker(groupId, recordId, {page: current.book.pageCount})
-    }
-
+    
     return (
         <Fragment>
             <div className="card book-card">
@@ -118,14 +64,6 @@ const Record = ({ recordState, markerState, setCurrentRecord, getMarkersFor, add
                      {current && current.book.publisher && <p><span className='volume-info'>Publisher: </span>{current.book.publisher}</p>}
                      {current && current.book.publishedOn && <p><span className='volume-info'>Published on: </span>{current.book.publishedOn.split("T")[0]}</p>}
                      {current && current.book.industryIdentifiers && <p><span className='volume-info'>{current.book.industryIdentifiers[0].type}: </span>{current.book.industryIdentifiers[0].identifier}</p>}
-                     {current && current.book.pageCount && <p><span className='volume-info'>Status: </span>
-                     <button className='link-style-btn' onClick={showNewMarkerModal}>{currentPage}</button>/{current.book.pageCount} pages. (<button className='link-style-btn' onClick={showStatusHistoryModal}>See full history</button>).</p>}
-                     {current && current.book.pageCount && (
-                        <div className='progress-bar-container'>
-                            <ProgressBar bgcolor={"#6a1b9a"} completed={completed}/>
-                            <button className='btn btn-green' onClick={markComplete}>Mark complete</button>
-                        </div>
-                     )}
                     </div>
                 </div>
                 <div className='btn-group'>
@@ -133,9 +71,10 @@ const Record = ({ recordState, markerState, setCurrentRecord, getMarkersFor, add
                     <button className='btn btn-green' onClick={showNewPostModal}>Write post</button>
                 </div>
             </div>
+            <div className='card'>
+                <Progress/>
+            </div>
             <Items/>
-            <Modal show={statusHistoryModal.show} handleClose={hideStatusHistoryModal} Component={StatusHistory}/>
-            <Modal show={newMarkerModal.show} handleClose={hideNewMarkerModal} Component={NewMarkerForm}/>
             <Modal show={newBookmarkModal.show} handleClose={hideNewBookmarkModal} Component={NewBookmarkForm}/>
             <Modal show={newPostModal.show} handleClose={hideNewPostModal} Component={NewPostForm}/>
         </Fragment>
@@ -144,19 +83,13 @@ const Record = ({ recordState, markerState, setCurrentRecord, getMarkersFor, add
 
 Record.propTypes = {
     recordState: PropTypes.object.isRequired,
-    markerState: PropTypes.object.isRequired,
-    setCurrentRecord: PropTypes.func.isRequired,
-    getPostsFor: PropTypes.func.isRequired,
-    getMarkersFor: PropTypes.func.isRequired,
-    getBookmarksFor: PropTypes.func.isRequired,
-    addMarker: PropTypes.func.isRequired
+    setCurrentRecord: PropTypes.func.isRequired
 }
 
 const mapStateToProps = state => ({
-    recordState: state.record,
-    markerState: state.marker
+    recordState: state.record
 })
 
-const addState = connect( mapStateToProps, {setCurrentRecord, getPostsFor, getMarkersFor, getBookmarksFor, addMarker});
+const addState = connect( mapStateToProps, {setCurrentRecord });
 
 export default addState(Record);
--- client/src/components/home/dashboard/group/record/Progress.js
+++ client/src/components/home/dashboard/group/record/Progress.js
@@ -1,24 +1,59 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect, Fragment } from 'react';
+import { useParams } from 'react-router-dom';
 import { connect } from "react-redux";
+import Spinner from '../../../../Spinner';
+import ProgressBar from './ProgressBar';
+import Modal from "../../../../Modal";
+import PropTypes from 'prop-types';
+import { getMarkersFor, addMarker } from '../../../../../actions/markerActions';
+import StatusHistory from './StatusHistory';
+import NewMarkerForm from './NewMarkerForm';
 
-const Progress = ({recordState, addMarker}) => {
+const Progress = ({recordState, markerState, getMarkersFor, addMarker}) => {
 
     const [newMarkerModal, setNewMarkerModal] = useState({show: false})
     const [statusHistoryModal, setStatusHistoryModal] = useState({show: false})
 
+    const showNewMarkerModal = () => {
+        setNewMarkerModal({
+            ...newMarkerModal,
+            show: true
+        })
+    }
+    const hideNewMarkerModal = () => {
+        setNewMarkerModal({
+            ...newMarkerModal,
+            show: false
+        })
+    }
+
+    
+    const showStatusHistoryModal = () => {
+        setStatusHistoryModal({
+            ...statusHistoryModal,
+            show: true
+        })
+    }
+
+    const hideStatusHistoryModal = () => {
+        setStatusHistoryModal({
+            ...statusHistoryModal,
+            show: false
+        })
+    }
+
     const {groupId, recordId} = useParams();
-    const { records } = recordState;
+    const { records, current } = recordState;
     const record = records.find(record => record._id === recordId);
 
     useEffect(() => {
         if(record){
-            setCurrentRecord(record);
             getMarkersFor(groupId, record);
         }
     }, [record])
 
     
-    const { markers } = markerState;
+    const { markers, loading } = markerState;
 
     let currentPage = 0;
     if (markers.length > 0){
@@ -37,17 +72,39 @@ const Progress = ({recordState, addMarker}) => {
         addMarker(groupId, recordId, {page: current.book.pageCount})
     }
 
+    if (loading){
+        return <Spinner/>
+    }
+
     return (
-        <div>
-            
-        </div>
+        <Fragment>
+            <h3>Progress</h3>
+            {current && current.book.pageCount && <p><span className='volume-info'>Status: </span>
+            <button className='link-style-btn' onClick={showNewMarkerModal}>{currentPage}</button>/{current.book.pageCount} pages. (<button className='link-style-btn' onClick={showStatusHistoryModal}>See full history</button>).</p>}
+            {current && current.book.pageCount && (
+            <div className='progress-bar-container'>
+                <ProgressBar bgcolor={"#6a1b9a"} completed={completed}/>
+                <button className='btn btn-green' onClick={markComplete}>Mark complete</button>
+            </div>
+            )}
+            <Modal show={statusHistoryModal.show} handleClose={hideStatusHistoryModal} Component={StatusHistory}/>
+            <Modal show={newMarkerModal.show} handleClose={hideNewMarkerModal} Component={NewMarkerForm}/>
+        </Fragment>
     )
 }
 
+Progress.propTypes = {
+    recordState: PropTypes.object.isRequired,
+    markerState: PropTypes.object.isRequired,
+    getMarkersFor: PropTypes.func.isRequired,
+    addMarker: PropTypes.func.isRequired
+}
+
 const mapStateToProps = state => ({
-    recordState: state.record
+    recordState: state.record,
+    markerState: state.marker
 })
 
-const addState = connect(mapStateToProps, {addMarker})
+const addState = connect(mapStateToProps, { getMarkersFor, addMarker })
 
 export default addState(Progress);
