aa699d0c06104c644609bf0b7d0722da949cc7ca
Arturo Javier-Castellanos <a.javier.castellanos@gmail.com>
2022-01-03 01:18:35 -0600

    Solve version conflic

--- routes/users.js
+++ routes/users.js
@@ -3,14 +3,14 @@ const router = express.Router({ mergeParams: true });
 const catchAsync = require("../utils/catchAsync");
 const { registerUser, changePassword, updatePicture } = require("../controllers/users");
 const { validateRegister, authenticate } = require("../middleware");
-// const { storage } = require("../cloudinary")
-// const multer = require("multer");
-// const upload = multer({ storage });
+const { storage } = require("../cloudinary")
+const multer = require("multer");
+const upload = multer({ storage });
 
 // // api/users
 
 router.post("/", catchAsync(validateRegister), catchAsync(registerUser));
 router.put("/:userId/change-password", authenticate, catchAsync(changePassword));
-// router.put("/:userId/update-picture", authenticate, upload.single("file"), catchAsync(updatePicture))
+router.put("/:userId/update-picture", authenticate, upload.single("file"), catchAsync(updatePicture))
 
 module.exports = router;
