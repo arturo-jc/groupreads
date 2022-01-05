e91f2b53a25cd2fc8f19f3ef1079992c75f68de7
Arturo Javier-Castellanos <a.javier.castellanos@gmail.com>
2022-01-05 09:43:30 -0600

    Fix invite instructions

--- client/src/components/home/dashboard/group/groupDetails/InviteFriends.js
+++ client/src/components/home/dashboard/group/groupDetails/InviteFriends.js
@@ -8,11 +8,11 @@ const InviteFriends = () => {
             <h3>Invite friends</h3>
             <p>To invite a friend to join this group:
                 <ol>
-                    <li>Give this code to your friend: { groupId }</li>
                     <li>Have your friend create an account if they don't have one already.</li>
-                    <li>Have your friend click on "Find group" and enter the code.</li>
-                    <li>Click them click on "join" to send a request.</li>
-                    <li>Once your friend sends a request, you should be able to see it below "Members" next time you log in or refresh your browser.</li>
+                    <li>Give them this code: { groupId }</li>
+                    <li>Have them click on "Find group" and enter the code.</li>
+                    <li>Have them click on "join" to send a request.</li>
+                    <li>Once your friend sends a request, you should be able to see it in the current page next time you log in or refresh your browser.</li>
                     <li>Click on "accept".</li>
                     <li>Done! Next time your friend logs in or refreshes their browser, they should be able to see this group.</li>
                 </ol>
