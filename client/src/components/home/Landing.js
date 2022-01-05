6954d5c9efb91a005bac5df96f3d3815ba674e57
Arturo Javier-Castellanos <a.javier.castellanos@gmail.com>
2022-01-04 23:28:51 -0600

    Finish landing

--- client/src/components/home/Landing.js
+++ client/src/components/home/Landing.js
@@ -13,35 +13,63 @@ const Landing = () => {
                     </div>
                 </header>
                 <div className='wrapper'>
-                    <div className="row">
+                    <div className="row-reverse">
                         <div className="col-50 pad">                        
                         <img className='screenshot' src={screenshot} alt="" />   
                         </div>
                         <div className="col-50 pad">
-                            <div className="card extend">
-                                <h3>Make a reading group</h3>
-                                <p>GroupReads makes it eady to...</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className='section-two'>
-            <div className='wrapper'>
-                    <div className="row">
-                        <div className="col-50 pad">
-                            <div className="card app-description">
-                                <h3>Make a reading group</h3>
+                            <div className="card extend app-description">
+                            <h3>Make a reading group</h3>
                                 <p>GroupReads makes it easy to create a group where you and your friends can keep track of all the books you read together.
                                 </p>
                             </div>
                         </div>
-                        <div className="col-50 pad">
-                            <img src={footage} alt="" />
-                        </div>
                     </div>
                 </div>
             </section>
+                    <section className='section-two'>
+                        <div className="wrapper">
+                            <div className="row">
+                                <div className="col-30 pad">
+                                    <div className="card extend feature-card">
+                                        <i className="fas fa-book fa-4x"></i>
+                                        <h3>Browse millions of books</h3>
+                                    <p>GroupReads is powered by Google Books. If it exists, there is a good chance you'll find it here!</p>                            </div>
+                                </div>
+                                <div className="col-30 pad">
+                                    <div className="card extend feature-card">
+                                        <i class="fas fa-mobile-alt fa-4x"></i>
+                                        <h3>Mobile friendly</h3>
+                                        <p>Access your groups from anywhere with an internet connection!</p>
+                                    </div>
+                                </div>
+                                <div className="col-30 pad">
+                                    <div className="card extend feature-card">
+                                    <i class="fas fa-wallet fa-4x"></i>
+                                        <h3>Completely free</h3>
+                                        <p>GroupReads is both free to use and free from ads!</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                <section className='section-three'>
+                <div className="fake-element"></div>
+                    <div className="wrapper">
+                    <div className="row">
+                            <div className="col-50 pad">                        
+                            <img className='screenshot' src={footage} alt="" />   
+                            </div>
+                            <div className="col-50 pad">
+                                <div className="card extend app-description">
+                                <h3>Stay on top of it</h3>
+                                    <p>Keep track of your progress with a progress bar, bookmarks, posts, and more!
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
         </Fragment>
     )
 }
